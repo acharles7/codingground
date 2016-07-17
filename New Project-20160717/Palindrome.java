39df12d33a5d9a7c88e6c079881158f276004ca0
acharles7 <charlespatel007@yahoo.com>
2016-07-17 12:21:23 +0530

    Created by Coding Ground Client

---
+++ New Project-20160717/Palindrome.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Palindrome{
+
+public static void main(String []args){
+    String original,reverse="";
+    
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Enter String : ");
+    original = sc.nextLine();
+    int l = original.length();
+    
+    for(int i=l-1;i>=0;i--){
+        reverse = reverse + original.charAt(i);
+    }
+    
+    if(original.equals(reverse))
+    System.out.println("String is Palindrome");
+    else
+    System.out.println("String is NOT Palindrome");
+ 
+ 
+ 
+ 
+ 
+    
+}
+}
